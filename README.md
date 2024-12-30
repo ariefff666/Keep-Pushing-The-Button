1520b6940cd70cbadfe0cf5dbdca3de67c63b3e1
ariefff666 <mariefpratama90@gmail.com>
2024-12-30 13:08:57 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# keep_pushing_the_button
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/keep_pushing_the_button/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.keep_pushing_the_button
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
